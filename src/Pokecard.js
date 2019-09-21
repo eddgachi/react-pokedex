13d60c4c63941c4e20c8957ac7202b88b3bc43da
Edwin Gachi <edwingachira@gmail.com>
2019-09-21 19:06:03 +0300

    finished first react app

---
+++ src/Pokecard.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Pokecard.css'
+
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/"
+
+let PadToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num);
+
+class Pokecard extends Component {
+    render() {
+      let ImgSrc = `${POKE_API}${PadToThree(this.props.id)}.png`
+        return (
+          <div className="Pokecard">
+            <h1 className="Pokecard-title">{this.props.name}</h1>
+            <img src={ImgSrc} alt={this.props.name} />
+            <div className="Pokecard-data">Type: {this.props.type}</div>
+            <div className="Pokecard-data">Exp: {this.props.exp}</div>
+          </div>
+        );
+    }
+}
+
+export default Pokecard;
